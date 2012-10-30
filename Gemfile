8cd443079c78012b12daa4ce57b5cbacb88dd53e
Kurt Nelson <kurtisnelson@gmail.com>
2012-10-30 11:11:47 -0400

    Allow pry in development

--- Gemfile
+++ Gemfile
@@ -9,4 +9,5 @@ group :development do
   gem "bundler", ">= 1.0.0"
   gem "jeweler", ">= 1.8.4"
   gem "simplecov", ">= 0"
+  gem "pry"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'simplecov'
 SimpleCov.start
+require 'pry'
 require 'rspec'
 require 'resumator'
 
