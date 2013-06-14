9158ee7ea5ad56cd526c125e3f527a1f6ae81828
Kurt Nelson <kurtisnelson@gmail.com>
2013-06-14 16:21:19 -0400

    Coveralls for rails

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear! 'rails'
 require 'pry'
 require 'rspec'
 require 'vcr'
